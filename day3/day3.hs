7961ce91e325a6d9aab33b1a7b33962b8110c544
Christian Pardillo Laursen <ninja1511@gmail.com>
2019-12-04 10:37:32 +0000

    Solve day 3

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,4 +1,4 @@
-import Data.Array
+import qualified Data.Map.Strict as Map
 import Data.List.Split (wordsBy)
 import System.IO
 
@@ -12,26 +12,54 @@ sol1 :: IO ()
 sol1 = do
     input <- readFile "inp3.txt"
     let inpList = map ((map toCoord) . wordsBy (==',')) $ lines input
-    print $ show $ accumArray (||) False (getMaxArrayBounds $ head inpList) []
-    print $ show $ map getWireArray inpList
+    print $ show $ minimum $ map toManhattan $ findIntersections 
+        (getPath $ head inpList)
+        (toPositionMap $ getPath $ last inpList)
+
+sol2 :: IO ()
+sol2 = do
+    input <- readFile "inp3.txt"
+    let inpList = map ((map toCoord) . wordsBy (==',')) $ lines input
+    print $ show $ minimum $ findStepIntersections 
+        (zip (getPath $ head inpList) [1..])
+        (toPositionStepMap $ getPath $ last inpList)
 
 toCoord :: String -> Coord
 toCoord s = (read ([head s]), read $ tail s)
 
-getMaxArrayBounds :: [Coord] -> ((Int, Int), (Int, Int))
-getMaxArrayBounds xs = (
-    (negate $ sum $ map snd $ filter ((==L) . fst) xs,
-    sum $ map snd $ filter ((==R) . fst) xs),
-    (negate $ sum $ map snd $ filter ((==D) . fst) xs,
-    sum $ map snd $ filter ((==U) . fst) xs))
+coordStep :: Coord -> (Int, Int) -> [(Int, Int)]
+coordStep (d, n) (x, y) = case d of
+    U -> [(x,y+n') | n' <- [1..n]]
+    D -> [(x,y-n') | n' <- [1..n]]
+    R -> [(x+n',y) | n' <- [1..n]]
+    L -> [(x-n',y) | n' <- [1..n]]
 
-getWireArray :: [Coord] -> Array (Int, Int) Bool
-getWireArray xs = helper xs (0,0) $ accumArray (||) False (getMaxArrayBounds xs) []
+getPath :: [Coord] -> [(Int, Int)]
+getPath cs = helper cs (0,0)
     where
-        helper [] _ arr = arr
-        helper (c:cs) (x,y) arr = case fst c of
-            U -> helper cs (x, y+snd c) (arr // [((x, y+n), True) | n <- [0..snd c]])
-            D -> helper cs (x, y-snd c) (arr // [((x, y-n), True) | n <- [0..snd c]])
-            L -> helper cs (x-snd c, y) (arr // [((x-n, y), True) | n <- [0..snd c]])
-            R -> helper cs (x+snd c, y) (arr // [((x+n, y), True) | n <- [0..snd c]])
+        helper :: [Coord] -> (Int, Int) -> [(Int, Int)]
+        helper [] p = []
+        helper (c:cs) p =
+            let step = coordStep c p
+            in step ++ (helper cs (last step))
 
+toPositionMap :: [(Int, Int)] -> Map.Map (Int, Int) ()
+toPositionMap = Map.fromList . (map (\x -> (x,())))
+
+toPositionStepMap :: [(Int, Int)] -> Map.Map (Int,Int) Int
+toPositionStepMap = Map.fromList . flip zip [1..]
+
+findIntersections :: [(Int, Int)] -> Map.Map (Int, Int) () -> [(Int, Int)]
+findIntersections [] _ = []
+findIntersections (x:xs) map = case Map.lookup x map of
+                                 Nothing -> findIntersections xs map
+                                 Just () -> x : findIntersections xs map
+
+findStepIntersections :: [((Int, Int), Int)] -> Map.Map (Int, Int) Int -> [Int]
+findStepIntersections [] _ = []
+findStepIntersections (x:xs) map = case Map.lookup (fst x) map of
+                                 Nothing -> findStepIntersections xs map
+                                 Just n -> (n + snd x) : findStepIntersections xs map
+
+toManhattan :: (Int, Int) -> Int
+toManhattan (x,y) = (abs x) + (abs y)
