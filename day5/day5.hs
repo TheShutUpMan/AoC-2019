ad4f528700676523d33e5111e91064f70f6403f2
Christian Pardillo Laursen <ninja1511@gmail.com>
2019-12-07 12:14:12 +0000

    Solve day 5

--- day5/day5.hs
+++ day5/day5.hs
@@ -14,6 +14,10 @@ data Opcode =
     | Mult Int Int Int
     | Inp  Int
     | Out  Int
+    | JmpT Int Int
+    | JmpF Int Int
+    | LTi   Int Int Int
+    | EQi   Int Int Int
     | Halt deriving (Show, Eq)
 
 data Mode = Position | Immediate deriving (Show, Eq)
@@ -25,7 +29,13 @@ main = sol1
 sol1 :: IO ()
 sol1 = do
     prog <- parseIntcode <$> readFile "inp5.txt"
-    print $ showTrace prog
+    print $ showTrace prog -- set testInput to 1
+
+sol2 :: IO ()
+sol2 = sol1 -- set testInput to 5
+
+testInput :: Int
+testInput = 5
 
 parseIntcode :: String -> Seq Int
 parseIntcode = S.fromList . map read . wordsBy (==',')
@@ -46,14 +56,27 @@ parseOpcode (viewl -> i:<xs) = case i `mod` 100 of
         where pos :<| _ = xs
     4 -> Out pos
         where pos :<| _ = xs
+    5 -> JmpT bool pos
+        where bool :<| pos :<| _ = xs
+    6 -> JmpF bool pos
+        where bool :<| pos :<| _ = xs
+    7 -> LTi a b pos
+        where a :<| b :<| pos :<| _ = xs
+    8 -> EQi a b pos
+        where a :<| b :<| pos :<| _ = xs
     99 -> Halt
 
 parseInstruction :: Seq Int -> Int -> Instruction
 parseInstruction xs pc = 
     let 
         xs' = S.drop pc xs
-        i :<| _ = xs
+        i :<| _ = xs'
      in (parseModes (i `div` 100), parseOpcode xs')
+
+getValue :: Seq Int -> Mode -> Int -> Int
+getValue prog mode val
+  | mode == Immediate = val
+  | otherwise = prog `index` val
 
 intcodeStep :: Seq Int -> Int -> Writer [String] (Seq Int, Int)
 intcodeStep prog pc = 
@@ -61,20 +84,34 @@ intcodeStep prog pc =
     in writer $ case op of
          Add a b pos -> ((update pos (a'+b') prog, pc + 4), [])
              where
-                 a' = if head modes == Immediate 
-                         then a else prog `index` a
-                 b' = if head (tail modes) == Immediate 
-                         then b else prog `index` b
+                 a' = getValue prog (head modes) a
+                 b' = getValue prog (head $ tail modes) b
          Mult a b pos -> ((update pos (a'*b') prog, pc + 4), [])
              where
-                 a' = if head modes == Immediate 
-                         then a else prog `index` a
-                 b' = if head (tail modes) == Immediate 
-                         then b else prog `index` b
-         Inp pos -> ((update pos 1 prog, pc + 2), [])
-         Out pos -> ((prog, pc + 2), [show $ prog `index` pos'])
-             where pos' = if head modes == Immediate 
-                             then pos else prog `index` pos
+                 a' = getValue prog (head modes) a
+                 b' = getValue prog (head $ tail modes) b
+         Inp pos -> ((update pos testInput prog, pc + 2), [])
+         Out pos -> ((prog, pc + 2), [show $ getValue prog (head modes) pos])
+         JmpT bool pos -> ((prog, pc'), [])
+             where
+                 pc' = if getValue prog (head modes) bool /= 0
+                       then getValue prog (head $ tail modes) pos
+                       else pc + 3
+         JmpF bool pos -> ((prog, pc'), [])
+             where
+                 pc' = if getValue prog (head modes) bool == 0
+                       then getValue prog (head $ tail modes) pos
+                       else pc + 3
+         LTi a b pos -> ((update pos newval prog, pc + 4), [])
+             where
+                 a' = getValue prog (head modes) a
+                 b' = getValue prog (head $ tail modes) b
+                 newval = if a' < b' then 1 else 0
+         EQi a b pos -> ((update pos newval prog, pc + 4), [])
+             where
+                 a' = getValue prog (head modes) a
+                 b' = getValue prog (head $ tail modes) b
+                 newval = if a' == b' then 1 else 0
          Halt -> ((S.Empty, 0), [])
 
 runIntcode :: Seq Int -> Writer [String] (Seq Int, Int)
@@ -87,5 +124,3 @@ showTrace xs = takeWhile
     (\x -> (fst . fst) (runWriter x) /= S.Empty) 
     (iterate (>>= uncurry intcodeStep) (writer ((xs, 0), [])))
 
-testProgram :: Seq Int
-testProgram = S.fromList [1002,4,3,4,33]
